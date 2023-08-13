905e2a5e227cb457060332ebfc8b847a62cbb446
Jacob Sendzik <jake.sendzik@gmail.com>
2023-08-13 14:57:56 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Jacob",
+    streetAddress: "12184 Cannes",
+}
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    const employees = {...employee};
+    employees.streetAddress = value;
+    return employees;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAdress, value) {
+    employee.streetAddress = value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee, name) {
+    const employees = {...employee};
+    delete employees.name;
+    return employees;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+};
